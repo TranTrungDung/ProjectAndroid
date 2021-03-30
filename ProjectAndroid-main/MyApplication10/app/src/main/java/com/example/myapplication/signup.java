6f6723253e16f97185f155f9c46c5a7040987b80
phamthanhquan2411 <traituoirong2411@gmail.com>
2021-03-30 11:33:27 +0700

    haha

---
+++ ProjectAndroid-main/MyApplication10/app/src/main/java/com/example/myapplication/signup.java
@@ -0,0 +1,40 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import static android.view.View.GONE;
+
+public class signup extends AppCompatActivity {
+    private Button btnback;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        btnback = (Button)findViewById(R.id.btnback);
+        btnback.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fm = getSupportFragmentManager();
+                MikensicoFragment fragment = new MikensicoFragment();
+                fm.beginTransaction().replace(R.id.sign,fragment).commit();
+                finish();
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+    }
+}
