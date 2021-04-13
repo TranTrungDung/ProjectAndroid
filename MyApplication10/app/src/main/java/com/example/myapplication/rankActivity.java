bc9f2559df4f131392fef4883428005c49ce15a4
TranTrungDung <1851060109@e.tlu.edu.vn>
2021-04-13 11:44:05 +0700

    abc

--- MyApplication10/app/src/main/java/com/example/myapplication/rankActivity.java
+++ MyApplication10/app/src/main/java/com/example/myapplication/rankActivity.java
@@ -2,9 +2,11 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ListView;
 
@@ -23,7 +25,7 @@ public class rankActivity extends AppCompatActivity {
     SharedPreferences sharedpreferences;
     ListView listView;
     rankAdapter adapter ;
-    ImageButton imgX;
+    ImageButton imgarrowback;
     BaiHocHelper baiHocHelper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +34,13 @@ public class rankActivity extends AppCompatActivity {
         setContentView(R.layout.activity_rank);
         listView = (ListView)findViewById(R.id.listrank);
         ArrayList<rankList> arrayList = new ArrayList<>();
+        imgarrowback = (ImageButton) findViewById(R.id.image_arrow_back);
+        imgarrowback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
         baiHocHelper = new BaiHocHelper(this,"mikenco.sqlite",null,1);
         int Count = 0 ;
         Cursor data = baiHocHelper.GetData(" SELECT *  FROM user ORDER BY sumbill desc limit 5");
