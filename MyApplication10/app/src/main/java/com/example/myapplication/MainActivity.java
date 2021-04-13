69fad6240853fe19f3d2bef0bff0794ae51d55f2
thanhquan24112000 <75117271+thanhquan24112000@users.noreply.github.com>
2021-04-13 20:20:59 +0700

    fgffggf

--- MyApplication10/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication10/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -130,9 +130,9 @@ public class MainActivity extends AppCompatActivity {
 //        baiHocHelper.QueryData("INSERT INTO storage_product VALUES(null,'8','M','50')");
 //        baiHocHelper.QueryData("INSERT INTO storage_product VALUES(null,'8','L','50')");
 //        baiHocHelper.QueryData("INSERT INTO storage_product VALUES(null,'8','XL','50')");
-
 //
-//        baiHocHelper.QueryData("INSERT INTO user VALUES(null,'phamthanhquan2411@gmail.com','quan','12345','0376135632',)");
+//
+//        baiHocHelper.QueryData("INSERT INTO user VALUES(null,'phamthanhquan2411@gmail.com','quan','12345','0376135632',0)");
 //        baiHocHelper.QueryData("INSERT INTO user VALUES(null,'trantrungdung@gmail.com','dung','12345','012345678',0)");
 //        baiHocHelper.QueryData("INSERT INTO user VALUES(null,'deodeo@gmail.com','Quynh','12345','0987654321',0)");
 
